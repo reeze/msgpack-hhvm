2a5eeab14680c33587f7c57d08d0422a2ad5860d
Reeze Xia <reeze@php.net>
2014-05-10 18:29:33 +0800

    Move success cases to good folder

---
+++ test/good/002.php
@@ -0,0 +1,17 @@
+<?php
+if(!extension_loaded('msgpack')) {
+    dl('msgpack.' . PHP_SHLIB_SUFFIX);
+}
+
+function test($type, $variable) {
+    $serialized = msgpack_serialize($variable);
+    $unserialized = msgpack_unserialize($serialized);
+
+    echo $type, PHP_EOL;
+    echo bin2hex($serialized), PHP_EOL;
+    var_dump($unserialized);
+    echo $unserialized === $variable ? 'OK' : 'ERROR', PHP_EOL;
+}
+
+test('null', null);
+?>
